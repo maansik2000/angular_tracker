042066c58075b08a6640ad6e0b5ce9e1e99cf761
maansik2000 <maansik2000@gmail.com>
2021-06-09 20:09:17 +0530

    make it better

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,34 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text : 'Doctor Appointment',
+        day : 'May 5th at 2:30',
+        reminder: true,
+    },
+    {
+        id:2,
+        text : 'Django Tutorial',
+        day : 'May 5th at 6:30',
+        reminder: true,
+    },
+    {
+        id:3,
+        text : 'CSGO ',
+        day : 'May 5th at 10:30',
+        reminder: true,
+    },
+    {
+        id:4,
+        text : 'Mern Stack',
+        day : 'May 5th at 12:30',
+        reminder: true,
+    },
+    {
+        id:5,
+        text : 'Practical Assignment',
+        day : 'May 6th at 11:00',
+        reminder: true,
+    },
+]
